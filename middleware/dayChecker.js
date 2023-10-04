7aac2ddd9aecba9c7e99744b9639362fd36a15ca
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-10-04 09:38:03 -0400

    dayChecker function now using JSON file

--- middleware/dayChecker.js
+++ middleware/dayChecker.js
@@ -1,27 +1,40 @@
 const fs = require("fs");
 
-function dayChecker(currentDate) {
-  const filePath = "./dataTxtFiles/days.txt";
+function dayChecker(currentDate, storeName) {
+  const filePath = "dataTxtFiles/days.JSON";
   let storedDate;
   try {
-    storedDate = fs.readFileSync(filePath, "utf-8");
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      fs.writeFileSync(filePath, currentDate, "utf-8");
-      console.log("File created with initial date:", currentDate);
-      storedDate = currentDate;
+    const fileContent = fs.readFileSync(filePath, "utf-8"); //try to see if the file isn't empty
+    if (fileContent) {
+      let parsedFile = JSON.parse(fileContent);
+      if (parsedFile[storeName]) {
+        storedDate = parsedFile;
+      } else {
+        let appendedData = JSON.stringify({
+          [storeName]: { date: currentDate },
+          ...parsedFile,
+        });
+        fs.writeFileSync(filePath, appendedData, "utf-8");
+        storedDate = appendedData;
+      }
     } else {
-      throw err;
+      let initialData = JSON.stringify({ [storeName]: { date: currentDate } });
+      fs.writeFileSync(filePath, initialData, "utf-8");
     }
+  } catch (err) {
+    throw err;
   }
-  if (storedDate !== currentDate) {
-    fs.writeFile(filePath, currentDate, (err) => {
+  if (storedDate && storedDate[storeName]?.date !== currentDate) {
+    storedDate[storeName].date = currentDate;
+    fs.writeFile(filePath, JSON.stringify(storedDate), (err) => {
       if (err) throw err;
       console.log("file wrote");
     });
+
     return true;
   } else {
     return false;
   }
 }
+
 module.exports = { dayChecker };
